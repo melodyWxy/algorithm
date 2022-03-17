da6e13f1545e104685c9abd407dded4594090ad2
melodyWxy <18210711176@163.com>
2022-03-17 20:23:53 +0800

    feat: +链表反转

--- packages/链表/单链表.js
+++ packages/链表/单链表.js
@@ -2,7 +2,7 @@
  * @Author: 六弦(melodyWxy)
  * @Date: 2022-03-17 15:33:39
  * @LastEditors: 六弦(melodyWxy)
- * @LastEditTime: 2022-03-17 15:37:39
+ * @LastEditTime: 2022-03-17 20:23:19
  * @FilePath: /react18/algorithm/packages/链表/单链表.js
  * @Description: update here
  */
@@ -43,3 +43,42 @@ var hasCycle = function(head) {
     return false;
 }
 
+
+// 单链表反转
+// 基于栈
+//  var reverseList = function(head) {
+//     if (head == null || head.next == null) {
+//         return head;
+//     }
+//     const stack = [];
+//     while(head){
+//         stack.push(head);
+//         head = head.next;
+//     }
+//     let next = stack.pop();
+//     let res = next;
+//     while(stack.length){
+//         next.next = stack.pop();
+//         next = next.next;
+//     }
+//     next.next = null;
+//     return res;
+   
+// };
+// 双链表
+var reverseList = function(head) {
+    if (head == null || head.next == null) {
+        return head;
+    }
+    let result = null;
+    let pre = null;
+    while (head != null) {
+        //temp用来记录head前一个元素
+        pre = head;
+        head = pre.next;
+        pre.next = result;
+        //result为第二个节点的头节点
+        result = pre;
+    }
+    return result;
+};
